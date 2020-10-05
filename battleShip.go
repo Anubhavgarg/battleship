fcbc3d09ec1f8d57d3069b9674ccb1e63f750f8f
Anubhav Garg <anubhav.g@airmeet.com>
2020-10-05 17:55:58 +0530

    grid size function

--- battleShip.go
+++ battleShip.go
@@ -1,14 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s <input-file> <output-file>\n", os.Args[0])
+func getGridSizeFunction(fileReader *bufio.Reader) int {
+	size, error := fileReader.ReadString('\n')
+	if error != nil {
+		log.Println(error)
 		os.Exit(-1)
 	}
 
+	number, error := strconv.ParseInt(strings.TrimSpace(size), 10, 0)
+	if error != nil {
+		log.Println(error)
+		os.Exit(-1)
+	}
+
+	return int(number)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		os.Exit(-1)
+	}
+	inputFile, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+	defer inputFile.Close()
+	outputFile, err := os.Create(os.Args[2])
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+	defer outputFile.Close()
+	fileReader := bufio.NewReader(inputFile)
+	getSize := getGridSizeFunction(fileReader)
+	println(getSize)
 }
